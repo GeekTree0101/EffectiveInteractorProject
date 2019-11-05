9afada85ebb07fc8363a652e88e5c0031e7f9683
Geektree0101 <h2s1880@gmail.com>
2019-11-05 09:37:35 +0900

    update Worker

--- EffectiveInteractorProject/Scene/CardWorker.swift
+++ EffectiveInteractorProject/Scene/CardWorker.swift
@@ -12,49 +12,19 @@ import Codextended
 
 class CardWorker {
   
+  
+  
   public func getCard(id: Int) -> Promise<Card> {
     
     return Promise<Card> { seal in
-      let dummyJSON: [String: Any] = [
-        "id": 1,
-        "title": "this is card",
-        "content": "hello world"
-      ]
-      
-      let dummyData: Data = try! JSONSerialization.data(
-        withJSONObject: dummyJSON,
-        options: .prettyPrinted
-      )
-      
-      guard let card = try? dummyData.decoded() as Card else {
-        seal.reject(NSError.init(domain: "not found", code: 200, userInfo: nil))
-        return
-      }
-      
-      seal.fulfill(card)
+      seal.fulfill(Card.init(id: 1))
     }
   }
   
   public func getUser(id: Int) -> Promise<User> {
     
     return Promise<User> { seal in
-      
-      let dummyJSON: [String: Any] = [
-        "id": 100,
-        "username": "Geektree0101"
-      ]
-      
-      let dummyData: Data = try! JSONSerialization.data(
-        withJSONObject: dummyJSON,
-        options: .prettyPrinted
-      )
-      
-      guard let user = try? dummyData.decoded() as User else {
-        seal.reject(NSError.init(domain: "not found", code: 200, userInfo: nil))
-        return
-      }
-      
-      seal.fulfill(user)
+      seal.fulfill(User.init(id: 1))
     }
   }
 }
